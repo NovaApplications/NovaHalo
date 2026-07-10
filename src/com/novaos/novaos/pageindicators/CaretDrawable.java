615c7fd9d17d44f19dd85b87535981efae49de31
Chantz Brimm <chantzbrimm06@gmail.com>
2026-07-10 00:58:50 -0400

    fixed padding bug on phones and fixed check update button.

--- src/com/novaos/novaos/pageindicators/CaretDrawable.java
+++ src/com/novaos/novaos/pageindicators/CaretDrawable.java
@@ -23,7 +23,6 @@ import android.graphics.Paint;
 import android.graphics.Path;
 import android.graphics.PixelFormat;
 import android.graphics.drawable.Drawable;
-
 import androidx.annotation.NonNull;
 
 import com.novaos.novaos.R;
